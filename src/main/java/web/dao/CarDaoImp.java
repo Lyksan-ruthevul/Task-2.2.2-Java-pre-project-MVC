c543313105d59e7f3e399ba6df8a23fe1c7f1438
Lyksan-ruthevul <154101744+Lyksan-ruthevul@users.noreply.github.com>
2024-03-03 21:28:36 +0300

    Task-2.2.2-Java-pre-project-MVC

---
+++ src/main/java/web/dao/CarDaoImp.java
@@ -0,0 +1,25 @@
+package web.dao;
+
+
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class CarDaoImp implements CarDao {
+    List<Car> carList = new ArrayList<>();
+    {
+        carList.add(new Car(1, "Lada", "Red"));
+        carList.add(new Car(2, "BMW", "White"));
+        carList.add(new Car(3, "Toyota", "Yellow"));
+        carList.add(new Car(4, "MB", "Black"));
+        carList.add(new Car(5, "Volvo", "Blue"));
+    }
+
+    public List<Car> getList() {
+        return carList;
+    }
+}
---
+++ src/main/java/web/service/CarServiceImp.java
@@ -0,0 +1,26 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.CarDao;
+import web.model.Car;
+
+import java.util.List;
+
+
+@Service
+public class CarServiceImp implements CarService {
+
+    private final CarDao carDao;
+
+    @Autowired
+    public CarServiceImp(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
+    @Override
+    public List<Car> getCars(int count) {
+        return (count >= 5) ? carDao.getList() :
+                carDao.getList().stream().limit(count).toList();
+    }
+}
